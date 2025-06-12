f22b15a1191144e92c64be094e68f95aeb0fa521
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-12 16:04:28 +0900

    クラスとインスタンスの復習

--- src/main/kotlin/Human.kt
+++ src/main/kotlin/Human.kt
@@ -1,4 +1,17 @@
 package jp.ac.it_college.std.s24016.kotlin
+//クラスとインスタンスの復習
+class Human(val name: String) {
+    fun showName(name: String) = println(name)
+    fun showName()             = println(name)
+}
 
-class Human {
+fun main() {
+    /*
+    クラスを使う -> インスタンスを生成する必要がある
+    インスタンス -> クラス名() でコンストラクタを呼び出せる
+    */
+    val human = Human("this プロパティ")
+
+    human.showName("demo name")
+    human.showName()//引数なしはプロパティに入っている値(this プロパティ)を返す
 }
