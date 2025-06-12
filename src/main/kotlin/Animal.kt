775660e25ff5bf5f8ee92a532ab714c0c3a1ac96
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-12 16:05:45 +0900

    継承の復習

--- src/main/kotlin/Animal.kt
+++ src/main/kotlin/Animal.kt
@@ -1,4 +1,10 @@
 package jp.ac.it_college.std.s24016.kotlin
+//継承の復習
+//継承させたいときはクラス名の頭に open をつける
+open class Animal(val name: String) {
 
-class Animal {
+    fun showName() = println("name is ${name}")
+
+    //Dogクラスで何も記述がない場合に、ここが実行される
+    open fun cries() = println("default")
 }
