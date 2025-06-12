fa0bc5aff58625770c57bc7e4913f9ff0354b377
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-12 15:08:10 +0900

    ステップインの動作確認

--- src/main/kotlin/FunctionTest.kt
+++ src/main/kotlin/FunctionTest.kt
@@ -1,15 +1,22 @@
 package jp.ac.it_college.std.s24016.kotlin
-
+//デバッグのステップインの使い方と単一式の場合の関数の書き方
 fun main() {
+    val length = countLength("Server Side Kotlin")
+    displayMessage("Server Side Kotlin: ${length}")
 }
 /*
     【 関数の基本 】
     fun 関数名( 引数: 型 ): 戻り値の型
 */
+//単一式
+fun countLength(str: String): Int = str.length
+/*
 fun countLength(str: String): Int {
     return str.length
 }
+*/
 
+//println を格納してあるだけの関数
 fun displayMessage(message: String){
     println(message)
 }
---
+++ src/main/kotlin/LoopsTest.kt
@@ -0,0 +1,2 @@
+package jp.ac.it_college.std.s24016.kotlin
+
