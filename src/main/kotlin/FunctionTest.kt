d86b776328e6d7f48962c302c9245c840fcb34db
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-12 14:09:00 +0900

    first commit

---
+++ src/main/kotlin/FunctionTest.kt
@@ -0,0 +1,15 @@
+package jp.ac.it_college.std.s24016.kotlin
+
+fun main() {
+}
+/*
+    【 関数の基本 】
+    fun 関数名( 引数: 型 ): 戻り値の型
+*/
+fun countLength(str: String): Int {
+    return str.length
+}
+
+fun displayMessage(message: String){
+    println(message)
+}
