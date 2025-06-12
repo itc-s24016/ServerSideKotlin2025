4038a3b573a7e912ee859f1c428fe028a42a579a
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-12 16:05:59 +0900

    継承の復習(サブクラス)

--- src/main/kotlin/Dog.kt
+++ src/main/kotlin/Dog.kt
@@ -1,4 +1,11 @@
 package jp.ac.it_college.std.s24016.kotlin
-
-class Dog {
+//継承の復習(サブクラス)
+class Dog(name: String): Animal(name) {
+    override fun cries() = println("bow wow!")
+}
+fun main(){
+    val dog = Dog("子犬")
+    //cries() を消したりすると値が変わる("bow wow!" or "default")
+    dog.cries()
+    dog.showName()
 }
