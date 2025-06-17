eddd242714190a0fbf034d2b534ad838fef15b66
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-17 15:15:21 +0900

    デバッグ機能のステップインの使い方と単一式の場合の関数の書き方

---
+++ src/main/kotlin/chap01/FunctionTest.kt
@@ -0,0 +1,19 @@
+package jp.ac.it_college.std.s24016.kotlin
+//デバッグ機能のステップインの使い方と単一式の場合の関数の書き方
+fun main() {
+    val length = countLength("Server Side Kotlin")
+    displayMessage("Server Side Kotlin: ${length}")
+}
+/* 【 関数の基本 】
+    fun 関数名( 引数: 型 ): 戻り値の型
+*/
+//単一式
+fun countLength(str: String): Int = str.length
+/*
+fun countLength(str: String): Int {
+    return str.length
+}
+*/
+fun displayMessage(message: String){
+    println(message)
+}
