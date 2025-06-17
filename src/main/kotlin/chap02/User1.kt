06fcd37428e50a0f4670f9429a2eff87fab557c2
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-17 16:35:04 +0900

    エラーの原因が確定するまで停止中

--- src/main/kotlin/chap02/User1.kt
+++ src/main/kotlin/chap02/User1.kt
@@ -1,2 +1,19 @@
 package chap02
-
+//エラーの原因が確定するまで停止中
+//class User1 {
+//    var name: String = ""
+//
+//    //ゲッターセッター
+//    fun getName() : String {
+//        return name
+//    }
+//    fun setName(name: String) {
+//        this.name = name
+//    }
+//}
+////Kotlin のコンパイラがプロパティのアクセサリメソッドを生成するのでメソッドの宣言が重複するためエラーになる
+//fun main() {
+//    val user = User1()
+//    user.name = "NoName"
+//    println(user.name)
+//}
