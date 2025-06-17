94a9ec638f062b74805a5bd24b136818ea22d184
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-17 14:12:06 +0900

    リストと列挙型でデータを扱うときの違い(リスト)

--- src/main/kotlin/BadColor.kt
+++ src/main/kotlin/BadColor.kt
@@ -1,2 +1,23 @@
 package jp.ac.it_college.std.s24016.kotlin
+//リストと列挙型でデータを扱うときの違い(リスト)
+import kotlin.random.Random
 
+fun main() {
+    val colors =
+        listOf(
+            "red",
+            "green",
+            "blue",
+            "yellow"
+        )
+    //リストからランダムに取得
+    val randomSelect = Random.nextInt(colors.size)
+
+    println(when(colors[randomSelect]) {
+            "red" -> "赤が選択されました"
+            "green" -> "青が選択されました"
+            "blue" -> "緑が選択されました"
+            else -> "不明な色が選択されました" //ここを消すとエラーになる
+        })
+    //リストは必ず else をつけないといけない
+}
