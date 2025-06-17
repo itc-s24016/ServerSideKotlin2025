9eaa7622bd704cd2a46d23944ac923edec9ef06e
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-17 15:41:49 +0900

    式として記述する(when)

--- src/main/kotlin/chap02/WhenTest.kt
+++ src/main/kotlin/chap02/WhenTest.kt
@@ -1,2 +1,13 @@
 package chap02
+//式として記述する(when)
+fun main() {
+    println(getNumberText(100))
+    println(getNumberText(300))
+}
 
+fun getNumberText(num: Int): String =
+    when(num){
+        100 -> {"Equal to 100"}
+        200 -> {"Equal to 200"}
+        else-> {"Undefined value"}
+    }
