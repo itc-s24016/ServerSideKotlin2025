be5e6251666be751ff99fc9d7be4c54460594239
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-17 16:41:24 +0900

    User2 を Java で動かしてみる

--- src/main/java/chap02/User2JavaTest.java
+++ src/main/java/chap02/User2JavaTest.java
@@ -1,4 +1,15 @@
 package chap02;
-
+//User2 を Java で動かしてみる
 public class User2JavaTest {
+    public static void main(String[] args) {
+        User2 user = new User2(1);
+        user.setName("名無し２");
+
+        /*
+        setName はあるけど、
+        setId は id が val のためセッターが生成されず
+        setId が見つからないと判定される
+        */
+        //user.setId(2);
+    }
 }
