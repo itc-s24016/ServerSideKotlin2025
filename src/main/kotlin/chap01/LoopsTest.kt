55627156f21d102723b7448e7adf89a148661b1a
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-17 15:22:58 +0900

    繰り返し系の復習(while, for, repeat)

---
+++ src/main/kotlin/chap01/LoopsTest.kt
@@ -0,0 +1,30 @@
+package jp.ac.it_college.std.s24016.kotlin
+//繰り返し系の復習(while, for, repeat)
+fun main() {
+    var i = 1
+
+    println("( while loop )")
+    while (i < 10){
+        println("i in ${i}")
+        i++
+    }
+
+    println("\n( for loop 1 )")
+    for (i in 1..10) {
+        println("i in ${i}")
+    }
+
+    println("\n( for loop 2 )")
+    for (i in 1..10 step 2){
+        println("i in ${i}")
+    }
+
+    println("\n( for loop 3 )")
+    val list = listOf(1, 2, 5, 6, 10)
+    for (i in list){
+        println("i in ${i}")
+    }
+
+    println("\n( repeat loop function )")
+    repeat(10, {println("i is ${it}")})
+}
