ba869cdc211ac4ab477e82e19d70cfbeca91ba9e
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-17 16:43:32 +0900

    lateinit

--- src/main/kotlin/chap02/User3.kt
+++ src/main/kotlin/chap02/User3.kt
@@ -1,2 +1,21 @@
 package chap02
+//lateinit
+class User3 {
+    //var age: Int
+    //lateinit であとから初期値を決めることができる
+    lateinit var name: String
+}
 
+class BadUser3() {
+    //初期値を入れた場合
+    var name: String = ""
+}
+
+fun main(){
+    val user = User3()
+    user.name = "名無し"
+    println(user.name)
+
+    val badUser = BadUser3()
+    println(badUser.name)
+}
