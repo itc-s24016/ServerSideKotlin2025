8f9c8deb24481a7d479112e200bc83d99e31f796
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-17 16:34:44 +0900

    エラーの原因が確定するまで停止中

---
+++ src/main/java/chap02/User1JavaTest.java
@@ -0,0 +1,14 @@
+package chap02;
+//User1 を Java で動かしてみる
+//エラーの原因が確定するまで停止中
+public class User1JavaTest {
+    public static void main(String[] args) {
+//        User1 user = new User1();
+//
+//        //.name に main からはアクセス出来ない
+////        user.name = "No Name";
+//
+//        user.setName("名無し");
+//        System.out.println(user.getName());
+    }
+}
