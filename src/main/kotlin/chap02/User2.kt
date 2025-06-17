500402871ee1d2e98879397b2bf1ca7c24d3337b
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-17 16:42:57 +0900

    val でプロパティ宣言するということ

--- src/main/kotlin/chap02/User2.kt
+++ src/main/kotlin/chap02/User2.kt
@@ -1,4 +1,12 @@
 package chap02
+//val でプロパティ宣言するということ
+class User2 (val id: Int){
+    var name: String = ""
+}
 
-class User2 {
+fun main() {
+    val user = User2(1)
+    user.name = "名無し"
+    //val で宣言する = 読み取り専用のプロパティなので、変更されない
+    //故に、セッターが生成されない
 }
