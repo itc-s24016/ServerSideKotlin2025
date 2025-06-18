6bb6ca5417200d784e462a50afe661d106fa0264
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-18 14:10:31 +0900

    修正完了

--- src/main/java/chap02/User1JavaTest.java
+++ src/main/java/chap02/User1JavaTest.java
@@ -1,14 +1,11 @@
 package chap02;
-//User1 を Java で動かしてみる
-//エラーの原因が確定するまで停止中
+//修正完了
 public class User1JavaTest {
     public static void main(String[] args) {
-//        User1 user = new User1();
-//
-//        //.name に main からはアクセス出来ない
-////        user.name = "No Name";
-//
-//        user.setName("名無し");
-//        System.out.println(user.getName());
+        User1 user = new User1();
+//        user.name = "Nakasone"
+        // ↑ name フィールドに直接アクセスできない
+        user.setName("名無し");
+        System.out.println(user.getName());
     }
 }
