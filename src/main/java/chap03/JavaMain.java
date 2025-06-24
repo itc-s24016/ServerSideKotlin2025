2ca9524647ea8db1f6fa893f13dacf1c0c7305ac
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-24 15:53:09 +0900

    Kotlin のクラスのインスタンスを Java で生成する

---
+++ src/main/java/chap03/JavaMain.java
@@ -0,0 +1,8 @@
+package chap03;
+//Kotlin のクラスのインスタンスを Java で生成する
+public class JavaMain {
+    public static void main(String[] args) {
+        HelloByKotlin hello = new HelloByKotlin();
+        hello.printHello();
+    }
+}
